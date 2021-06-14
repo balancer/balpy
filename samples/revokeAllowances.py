0271eb4d777c9be5c63a6833764313ba25eb71b4
gerg <gerrrrrrrg@gmail.com>
2021-06-14 09:28:59 -0400

    update license in setup.cfg. add sample revokeAllowance script

---
+++ samples/revokeAllowances.py
@@ -0,0 +1,39 @@
+import balpy
+
+def main():
+	gasFactor = 1.05;
+	gasSpeed = "fast";
+
+	network = "kovan"
+	tokens = [	"0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1",
+				"0x41286Bb1D3E870f3F750eB7E1C25d7E48c8A1Ac7",
+				"0xc2569dd7d0fd715B054fBf16E75B001E5c0C1115",
+				"0xAf9ac3235be96eD496db7969f60D354fe5e426B0",
+				"0x04DF6e4121c27713ED22341E7c7Df330F56f289B",
+				"0x8F4beBF498cc624a0797Fe64114A6Ff169EEe078",
+				"0x1C8E3Bcb3378a443CC591f154c5CE0EBb4dA9648",
+				"0xcC08220af469192C53295fDd34CFb8DF29aa17AB",
+				"0x15E76Fc74C6ab1c3141D61219883d1c59F716E21",
+				"0x22ee6c3B011fACC530dd01fe94C58919344d6Db5"];
+
+	bal = balpy.balpy.balpy(network);
+	nonce = bal.web3.eth.get_transaction_count(bal.web3.eth.default_account);
+	hashes = [];
+	for token in tokens:
+		print(token)
+		txHash = bal.erc20SignAndSendNewAllowance(	token, 
+													bal.VAULT, 
+													0, 
+													gasFactor, 
+													gasSpeed, 
+													nonceOverride=nonce, 
+													isAsync=True,
+													gasPriceGweiOverride=5);
+		nonce += 1
+		hashes.append(txHash);
+	for txHash in hashes:
+		print(txHash)
+		bal.waitForTx(txHash);
+		
+if __name__ == '__main__':
+	main();
