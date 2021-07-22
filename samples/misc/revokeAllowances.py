d5c56c9b4642eeb706079829d38420c1c9282efd
gerg <gerrrrrrrg@gmail.com>
2021-07-22 14:36:42 -0400

    rename bal.VAULT to bal.deploymentAddress[Vault]

--- samples/misc/revokeAllowances.py
+++ samples/misc/revokeAllowances.py
@@ -6,6 +6,8 @@ def main():
 	gasOverride = 5;
 
 	network = "kovan"
+	bal = balpy.balpy.balpy(network);
+	allowedAddress = bal.deploymentAddresses["Vault"];
 	tokens = [	"0xdFCeA9088c8A88A76FF74892C1457C17dfeef9C1",
 				"0x41286Bb1D3E870f3F750eB7E1C25d7E48c8A1Ac7",
 				"0xc2569dd7d0fd715B054fBf16E75B001E5c0C1115",
@@ -17,15 +19,15 @@ def main():
 				"0x15E76Fc74C6ab1c3141D61219883d1c59F716E21",
 				"0x22ee6c3B011fACC530dd01fe94C58919344d6Db5"];
 
-	bal = balpy.balpy.balpy(network);
 	nonce = bal.web3.eth.get_transaction_count(bal.web3.eth.default_account);
 	hashes = [];
+
 	for token in tokens:
 		print("Checking:", token)
-		allowance = bal.erc20GetAllowanceStandard(token, bal.VAULT);
+		allowance = bal.erc20GetAllowanceStandard(token, allowedAddress);
 		if allowance > 0:
 			txHash = bal.erc20SignAndSendNewAllowance(	token,
-														bal.VAULT,
+														allowedAddress,
 														0,
 														gasFactor,
 														gasSpeed,
