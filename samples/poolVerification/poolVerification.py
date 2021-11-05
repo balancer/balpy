6e1f85c5451300b7a814d70eb051687247cc265c
gerg <gerrrrrrrg@gmail.com>
2021-11-05 15:36:04 -0400

    add pool verification script

---
+++ samples/poolVerification/poolVerification.py
@@ -0,0 +1,18 @@
+import balpy
+
+def main():
+	network = "kovan"
+
+	bal = balpy.balpy.balpy(network);
+	poolId = "0x0910c350d5e836324491892d51bb4e97c33eaf23000200000000000000000357"
+	command = bal.balGeneratePoolCreationArguments(poolId);
+	
+	print()
+	print(command)
+	print()
+
+	print("If you need more complete instructions on what to do with this command, go to:");
+	print("\thttps://dev.balancer.fi/resources/pools/verification");
+	
+if __name__ == '__main__':
+	main();
