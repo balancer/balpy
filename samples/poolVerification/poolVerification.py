6af43201ec107ed275bebde016cea5d3aa823eea
gerg <gerrrrrrrg@gmail.com>
2021-11-17 10:49:57 -0500

    add ability to verify using txhash

--- samples/poolVerification/poolVerification.py
+++ samples/poolVerification/poolVerification.py
@@ -1,18 +1,31 @@
 import balpy
 
 def main():
-	network = "kovan"
+	network = "polygon"
+	verbose = True;
 
 	bal = balpy.balpy.balpy(network);
-	poolId = "0x0910c350d5e836324491892d51bb4e97c33eaf23000200000000000000000357"
-	command = bal.balGeneratePoolCreationArguments(poolId);
+	poolId = "0x0d34e5dd4d8f043557145598e4e2dc286b35fd4f000000000000000000000068"
+
+	# On Ethereum and Ethereum testnets, you can pass creationHash=None
+	# On Polygon, you must pass the pool creation hash to generate verification params
+
+	# creationHash = None;
+	creationHash = "0x18c7e1c9235c6e93878e55a87ed249f9d0ceb9d12ee584794e92f80f7645686d";
+
+	isVerified = bal.isContractVerified(poolId, verbose=verbose);
+	if not isVerified:
+		command = bal.balGeneratePoolCreationArguments(poolId, verbose=verbose, creationHash=creationHash);
+	else:
+		print("Pool is already verified")
+		quit();
 	
 	print()
 	print(command)
 	print()
 
 	print("If you need more complete instructions on what to do with this command, go to:");
-	print("\thttps://dev.balancer.fi/resources/pools/verification");
+	print("\thttps://dev.balancer.fi/resources/pools/verification\n");
 	
 if __name__ == '__main__':
 	main();
