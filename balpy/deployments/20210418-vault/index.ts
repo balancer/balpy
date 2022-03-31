0f7701db30f9f0eb8c1fa19d26f0ad9bd61fa706
gerg <gerrrrrrrg@gmail.com>
2022-03-31 14:04:00 -0400

    remove unnecessary bytecode and other files

--- balpy/deployments/20210418-vault/index.ts
+++
@@ -1,9 +0,0 @@
-import Task from '../../src/task';
-import { VaultDeployment } from './input';
-import { TaskRunOptions } from '../../src/types';
-
-export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
-  const input = task.input() as VaultDeployment;
-  const args = [input.Authorizer, input.weth, input.pauseWindowDuration, input.bufferPeriodDuration];
-  await task.deployAndVerify('Vault', args, from, force);
-};
