032b313855fc90c569d095afb4e9ef80784ef5be
gerg <gerrrrrrrg@gmail.com>
2022-05-06 16:12:33 -0400

    add test script for deployment table generation

---
+++ samples/misc/generateDeploymentsDocsTable.py
@@ -0,0 +1,11 @@
+import balpy
+
+def main():
+	network = "kovan"
+
+	bal = balpy.balpy.balpy(network);
+	output = bal.generateDeploymentsDocsTable();
+	print(output)
+		
+if __name__ == '__main__':
+	main();
