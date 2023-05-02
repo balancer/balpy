a6133ddb19f3c28ddc46744fab4bb3dba30c4aaa
Pedro Yves Fracari <yves.fracari@gmail.com>
2023-05-02 19:11:30 -0300

    Add exitPool sample

---
+++ samples/exitPool/exitPoolSample.py
@@ -0,0 +1,53 @@
+import balpy
+import sys
+import os
+import jstyleson
+import json
+
+def main():
+	
+	if len(sys.argv) < 2:
+		print("Usage: python3", sys.argv[0], "/path/to/exitData.json")
+		quit()
+
+	pathToExit = sys.argv[1]
+	if not os.path.isfile(pathToExit):
+		print("Path", pathToExit, "does not exist. Please enter a valid path.")
+		quit()
+
+	with open(pathToExit) as f:
+		exitData = jstyleson.load(f)
+
+	gasFactor = 1.05
+	gasSpeed = "average"
+	gasPriceGweiOverride = -1 # -1 uses etherscan price at speed for gasSpeed, all other values will override
+	bal = balpy.balpy.balpy(exitData["network"])
+
+	print()
+	print("==============================================================")
+	print("================ Step 1: Check Bpt Balance  ==================")
+	print("==============================================================")
+	print()
+	
+	poolAddress = bal.balPooldIdToAddress(exitData["poolId"])
+	if not bal.erc20HasSufficientBalances([poolAddress], [exitData["bptAmount"]]):
+		print("Please fix your insufficient BPT balance before proceeding.")
+		print("Quitting...")
+		quit()
+
+	print()
+	print("===============================================================")
+	print("================= Step 2: Send Bpt to Pool =================")
+	print("===============================================================")
+	print()
+
+	query = False
+	exitData.pop("network")
+	output = bal.balExitPool(**exitData, query=query)
+
+	if query:
+		print("queryJoin results:")
+		print(json.dumps(output,indent=4))
+
+if __name__ == '__main__':
+	main()
