526edf53688f9562846e005235ab4b5ed577c451
Pedro Yves <pedrokretzschmar@Pedros-MacBook-Air.local>
2023-05-02 08:42:08 -0300

    draft: add exit pool tests

--- balpy_bleu/balpy/contract.py
+++ balpy_bleu/balpy/contract.py
@@ -5,11 +5,10 @@ from lib import get_web3_contract, get_web3_instance
 from chains import Chain
 from typing import List
 from enums import WeightedPoolExitKind
-from eth_account import Account
 
 
 class Vault:
-    def __init__(self, chain=Chain.mainnet):
+    def __init__(self, chain=Chain.goerli):
         self.chain = chain
         self.web3 = get_web3_instance()
         self.contract = get_web3_contract(chain.vault_address)
@@ -18,44 +17,49 @@ class Vault:
     def get_protocol_fees_collector(self):
         return self.contract.functions.getProtocolFeesCollector().call()
 
-
     def estimateGas(self):
         last_block = self.web3.eth.get_block("latest", full_transactions=True)
-        return int(sum([tx.gas for tx in last_block.transactions]) / len(last_block.transactions))
+        return int(
+            sum([tx.gas for tx in last_block.transactions])
+            / len(last_block.transactions)
+        )
 
     def balDoExitPool(self, poolId, address, exitPoolRequestTuple):
-        exitPoolFunction = self.contract.functions.exitPool(poolId, address, address, exitPoolRequestTuple)
-        tx = self.buildTx(exitPoolFunction)
+        exitPoolFunction = self.contract.functions.exitPool(
+            poolId, address, address, exitPoolRequestTuple
+        )
+        tx = self.buildTx(exitPoolFunction, address)
         return self.sendTx(tx)
 
-    def buildTx(self, fn):
+    def buildTx(self, fn, address):
         gasEstimate = self.estimateGas()
-        import pdb; pdb.set_trace()
-        return fn.build_transaction({
-            'chainId': self.chain.id,
-            'gas': gasEstimate,
-            'gasPrice': self.web3.eth.gas_price,
-            'nonce': self.web3.eth.get_transaction_count(self.chain.vault_address)
-        })
-    
+        return fn.build_transaction(
+            {
+                "chainId": self.chain.id,
+                "gas": gasEstimate,
+                "gasPrice": self.web3.eth.gas_price,
+                "nonce": self.web3.eth.get_transaction_count(address),
+            }
+        )
+
     def sendTx(self, tx):
-        signedTx = Account.sign_transaction(tx, self.private_key)
-        import pdb; pdb.set_trace()
+        signedTx = self.web3.eth.account.sign_transaction(tx, self.private_key)
         return self.web3.eth.send_raw_transaction(signedTx.rawTransaction)
 
     def exitPool(
-        self, 
+        self,
         poolId: str,
         address: str,
-        bptAmount: int,
+        bptAmount: float,
         tokenList: List[str],
-        exitType:str, 
-        tokenOut:int = None,
-        amountsOut:List[int] = [],
-        query:bool = False,
-        toInternalBalance:bool = True,
-        minAmountsOut:List[int] = None,
-        **balDoExitPoolKwargs):
+        exitType: str,
+        tokenOut: int = None,
+        amountsOut: List[int] = [],
+        query: bool = False,
+        toInternalBalance: bool = True,
+        minAmountsOut: List[float] = None,
+        **balDoExitPoolKwargs
+    ):
         """
         poolId: Id of the pool to exit
         address: Address of the user to burn BPTs from and receive tokens to
@@ -68,43 +72,57 @@ class Vault:
         query: If True, returns the transaction object without sending the transaction
         """
         address = self.web3.to_checksum_address(address)
-        requestedTuple = WeightedPoolExitKind[exitType].getExitPoolRequestTuple(self.web3, tokenList, bptAmount, tokenOut, toInternalBalance, minAmountsOut)
+        requestedTuple = WeightedPoolExitKind[exitType].getExitPoolRequestTuple(
+            self.web3,
+            tokenList,
+            bptAmount,
+            to_internal_balance=toInternalBalance,
+            min_amounts_out=minAmountsOut,
+            token_out=tokenOut,
+            amounts_out=amountsOut,
+        )
         return self.balDoExitPool(poolId, address, requestedTuple)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     vault = Vault()
     # test_exit_0 = {
     #     "poolId": "0x9f1f16b025f703ee985b58ced48daf93dad2f7ef000200000000000000000063".lower(),
     #     "address": "0x76b0340e50BD9883D8B2CA5fd9f52439a9e7Cf58".lower(),
-    #     "bptAmount": 0.01,
-    #     "tokenList": ["0xdfcea9088c8a88a76ff74892c1457c17dfeef9c1".lower(), "0xe0c9275e44ea80ef17579d33c55136b7da269aeb".lower()],
+    #     "bptAmount": 0.1,
+    #     "tokenList": [
+    #         "0xdfcea9088c8a88a76ff74892c1457c17dfeef9c1".lower(),
+    #         "0xe0c9275e44ea80ef17579d33c55136b7da269aeb".lower(),
+    #     ],
     #     "exitType": "EXACT_BPT_IN_FOR_ONE_TOKEN_OUT",
     #     "tokenOut": 0,
     # }
 
     # print(vault.exitPool(**test_exit_0))
 
-    test_exit_1 = {
+    # test_exit_1 = {
+    #     "poolId": "0x9f1f16b025f703ee985b58ced48daf93dad2f7ef000200000000000000000063".lower(),
+    #     "address": "0x76b0340e50BD9883D8B2CA5fd9f52439a9e7Cf58".lower(),
+    #     "bptAmount": 0.1,
+    #     "tokenList": [
+    #         "0xdfcea9088c8a88a76ff74892c1457c17dfeef9c1".lower(),
+    #         "0xe0c9275e44ea80ef17579d33c55136b7da269aeb".lower(),
+    #     ],
+    #     "exitType": "EXACT_BPT_IN_FOR_TOKENS_OUT",
+    # }
+
+    # print(vault.exitPool(**test_exit_1))
+
+    test_exit_2 = {
         "poolId": "0x9f1f16b025f703ee985b58ced48daf93dad2f7ef000200000000000000000063".lower(),
         "address": "0x76b0340e50BD9883D8B2CA5fd9f52439a9e7Cf58".lower(),
-        "bptAmount": 100000000,
-        "tokenList": ["0xdfcea9088c8a88a76ff74892c1457c17dfeef9c1".lower(), "0xe0c9275e44ea80ef17579d33c55136b7da269aeb".lower()],
-        "exitType": "EXACT_BPT_IN_FOR_TOKENS_OUT",
+        "bptAmount": 0.1,
+        "tokenList": [
+            "0xdfcea9088c8a88a76ff74892c1457c17dfeef9c1".lower(),
+            "0xe0c9275e44ea80ef17579d33c55136b7da269aeb".lower(),
+        ],
+        "amountsOut": [int(.001e18), int(5e6)],
+        "exitType": "BPT_IN_FOR_EXACT_TOKENS_OUT",
     }
 
     print(vault.exitPool(**test_exit_2))
-
-    # test_exit_2 = {
-    #     "poolId": "0x9f1f16b025f703ee985b58ced48daf93dad2f7ef000200000000000000000063".lower(),
-    #     "address": "0x76b0340e50BD9883D8B2CA5fd9f52439a9e7Cf58".lower(),
-    #     "bptAmount": 100000000,
-    #     "tokenList": ["0xdfcea9088c8a88a76ff74892c1457c17dfeef9c1".lower(), "0xe0c9275e44ea80ef17579d33c55136b7da269aeb".lower()],
-    #     "amountsOut":[],
-    #     "exitType": "BPT_IN_FOR_EXACT_TOKENS_OUT",
-    # }
-
-    # print(vault.exitPool(**test_exit_2))
-
-    
--- balpy_bleu/balpy/enums.py
+++ balpy_bleu/balpy/enums.py
@@ -1,120 +1,170 @@
 import eth_abi
 from typing import List
+from decimal import *
+
 
 from abc import ABC, abstractmethod, abstractproperty
 import enum
 
+
 class WeightedPoolExitKindAbstracted(ABC):
-  @staticmethod
-  def getDefaultMinAmountsOut(token_list):
-    return [0] * len(token_list)
+    @staticmethod
+    def getDefaultMinAmountsOut(token_list):
+        return [0] * len(token_list)
 
-  @staticmethod
-  def getPoolAddress(w3, poolId):
-    return w3._tochecksum_address(poolId[0:42])
+    @staticmethod
+    def getPoolAddress(w3, poolId):
+        return w3._tochecksum_address(poolId[0:42])
 
-  def converTokenToWei(self, token, amount):
-    # TODO:
-	def balConvertTokensToWei(self, tokens, amounts):
-		rawTokens = [];
-		if not len(tokens) == len(amounts):
-			self.ERROR("Array length mismatch with " + str(len(tokens)) + " tokens and " + str(len(amounts)) + " amounts.");
-			return(False);
-		numElements = len(tokens);
-		for i in range(numElements):
-			token = tokens[i];
-			rawValue = amounts[i];
-			decimals = self.erc20GetDecimals(token);
-			if rawValue == self.INFINITE or rawValue == self.MAX_UINT_112:
-				decimals = 0;
-			raw = int(Decimal(rawValue) * Decimal(10**decimals));
-			rawTokens.append(raw);
-		return(rawTokens);
+    def convertTokenToWei(self, token, amount):
+        decimals = 18  # CHANGE THIS - get from contract of token
+        return int(Decimal(amount) * Decimal(10**decimals))
 
-  @abstractproperty
-  def id():
-    pass
-  
-  @abstractmethod
-  def getUserData():
-    pass
+    def convertTokensToWei(self, tokens, amounts):
+        return [
+            self.convertTokenToWei(token, amount)
+            for token, amount in zip(tokens, amounts)
+        ]
 
-  @abstractmethod
-  def sortTokenList():
-    pass
+    @abstractproperty
+    def id():
+        pass
 
-  @abstractmethod
-  def getExitPoolRequestTuple():
-    pass
+    @abstractmethod
+    def getUserData():
+        pass
+
+    @abstractmethod
+    def sortTokenList():
+        pass
+
+    @abstractmethod
+    def getExitPoolRequestTuple():
+        pass
 
 
 class ExactBptInForOneTokenOut(WeightedPoolExitKindAbstracted):
-  id = 0
+    id = 0
 
-  def sortTokenList(self, w3, token_list, token_out, min_amounts_out):
-    token_list_address = [w3.to_checksum_address(token) for token in token_list]
-    min_amounts_out_sorted = [minAmountOut for _,minAmountOut in sorted(zip(token_list_address, min_amounts_out))]
-    token_out_address = w3.to_checksum_address(token_list_address[token_out])
-    token_list_address_sorted = sorted(token_list_address)
-    token_out_sorted = token_list_address_sorted.index(token_out_address)
-    return token_list_address_sorted, token_out_sorted, min_amounts_out_sorted
+    def sortTokenList(self, w3, token_list, token_out, min_amounts_out):
+        token_list_address = [w3.to_checksum_address(token) for token in token_list]
+        min_amounts_out_sorted = [
+            minAmountOut
+            for _, minAmountOut in sorted(zip(token_list_address, min_amounts_out))
+        ]
+        token_out_address = w3.to_checksum_address(token_list_address[token_out])
+        token_list_address_sorted = sorted(token_list_address)
+        token_out_sorted = token_list_address_sorted.index(token_out_address)
+        return token_list_address_sorted, token_out_sorted, min_amounts_out_sorted
 
-  def getUserData(self, bpt_amount, token_out):
-    return eth_abi.encode(['uint256', 'uint256', 'uint256'], [self.id, bpt_amount, token_out])  
+    def getUserData(self, bpt_amount, token_out):
+        return eth_abi.encode(
+            ["uint256", "uint256", "uint256"], [self.id, bpt_amount, token_out]
+        )
 
-  def getExitPoolRequestTuple(self, w3, token_list, bpt_amount, token_out, to_internal_balance: bool = True, min_amounts_out: List[int]= None):
-    if min_amounts_out is None:
-      min_amounts_out = self.getDefaultMinAmountsOut(token_list)
-    token_list, token_out, min_amounts_out = self.sortTokenList(w3, token_list, token_out, min_amounts_out)
-    user_data = self.getUserData(bpt_amount, token_out)
-    return (token_list, min_amounts_out, user_data, to_internal_balance)
+    def getExitPoolRequestTuple(
+        self,
+        w3,
+        token_list,
+        bpt_amount,
+        token_out,
+        to_internal_balance: bool = True,
+        min_amounts_out: List[int] = None,
+        **kwargs,
+    ):
+        if min_amounts_out is None:
+            min_amounts_out = self.getDefaultMinAmountsOut(token_list)
+        bpt_amount = self.convertTokenToWei("", bpt_amount)
+        token_list, token_out, min_amounts_out = self.sortTokenList(
+            w3, token_list, token_out, min_amounts_out
+        )
+        user_data = self.getUserData(bpt_amount, token_out)
+        return (token_list, min_amounts_out, user_data, to_internal_balance)
+
 
 class ExactBptInForTokensOut(WeightedPoolExitKindAbstracted):
-  id = 1
+    id = 1
 
-  def sortTokenList(self, w3, token_list, min_amounts_out):
-    token_list_address = [w3.to_checksum_address(token) for token in token_list]
-    min_amounts_out_sorted = [minAmountOut for _,minAmountOut in sorted(zip(token_list_address, min_amounts_out))]
-    token_list_address_sorted = sorted(token_list_address)
-    return token_list_address_sorted, min_amounts_out_sorted
+    def sortTokenList(self, w3, token_list, min_amounts_out):
+        token_list_address = [w3.to_checksum_address(token) for token in token_list]
+        min_amounts_out_sorted = [
+            minAmountOut
+            for _, minAmountOut in sorted(zip(token_list_address, min_amounts_out))
+        ]
+        token_list_address_sorted = sorted(token_list_address)
+        return token_list_address_sorted, min_amounts_out_sorted
 
-  def getUserData(self, bpt_amount):
-    return eth_abi.encode(['uint256', 'uint256'], [self.id, bpt_amount])  
+    def getUserData(self, bpt_amount):
+        return eth_abi.encode(["uint256", "uint256"], [self.id, bpt_amount])
 
-  def getExitPoolRequestTuple(self, w3, token_list, bpt_amount, to_internal_balance: bool = True, min_amounts_out: List[int]= None):
-    if min_amounts_out is None:
-      min_amounts_out = self.getDefaultMinAmountsOut(token_list)
-    token_list, min_amounts_out = self.sortTokenList(w3, token_list, min_amounts_out)
-    user_data = self.getUserData(bpt_amount)
-    return (token_list, min_amounts_out, user_data, to_internal_balance)
+    def getExitPoolRequestTuple(
+        self,
+        w3,
+        token_list,
+        bpt_amount,
+        to_internal_balance: bool = True,
+        min_amounts_out: List[int] = None,
+        **kwargs,
+    ):
+        if min_amounts_out is None:
+            min_amounts_out = self.getDefaultMinAmountsOut(token_list)
+        bpt_amount = self.convertTokenToWei("", bpt_amount)
+        token_list, min_amounts_out = self.sortTokenList(
+            w3, token_list, min_amounts_out
+        )
+        user_data = self.getUserData(bpt_amount)
+        return (token_list, min_amounts_out, user_data, to_internal_balance)
+
 
 class BptInForExactTokensOut(WeightedPoolExitKindAbstracted):
-  id = 2
+    id = 2
 
-  def sortTokenList(self, w3, token_list, amounts_out, min_amounts_out):
-    token_list_address = [w3.to_checksum_address(token) for token in token_list]
-    min_amounts_out_sorted = [minAmountOut for _,minAmountOut in sorted(zip(token_list_address, min_amounts_out))]
-    amounts_out_sorted = [amountOut for _,amountOut in sorted(zip(token_list_address, amounts_out))]
-    token_list_address_sorted = sorted(token_list_address)
-    return token_list_address_sorted, amounts_out_sorted, min_amounts_out_sorted
+    def sortTokenList(self, w3, token_list, amounts_out, min_amounts_out):
+        token_list_address = [w3.to_checksum_address(token) for token in token_list]
+        min_amounts_out_sorted = [
+            minAmountOut
+            for _, minAmountOut in sorted(zip(token_list_address, min_amounts_out))
+        ]
+        amounts_out_sorted = [
+            amountOut for _, amountOut in sorted(zip(token_list_address, amounts_out))
+        ]
+        token_list_address_sorted = sorted(token_list_address)
+        return token_list_address_sorted, amounts_out_sorted, min_amounts_out_sorted
 
-  def getUserData(self, amounts_out, bpt_amount):
-    return eth_abi.encode(['uint256', 'uint256[]', ], [self.id, amounts_out, bpt_amount])  
+    def getUserData(self, amounts_out, bpt_amount):
+        return eth_abi.encode(
+            [
+                "uint256",
+                "uint256[]",
+                "uint256",
+            ],
+            [self.id, amounts_out, bpt_amount],
+        )
 
-  def getExitPoolRequestTuple(self, w3, token_list, bpt_amount, amounts_out, to_internal_balance: bool = True, min_amounts_out: List[int]= None):
-    if min_amounts_out is None:
-      min_amounts_out = self.getDefaultMinAmountsOut(token_list)
-    token_list, amounts_out, min_amounts_out = self.sortTokenList(w3, token_list, amounts_out, min_amounts_out)
-    user_data = self.getUserData(amounts_out, bpt_amount)
-    return (token_list, min_amounts_out, user_data, to_internal_balance)
-
-
+    def getExitPoolRequestTuple(
+        self,
+        w3,
+        token_list,
+        bpt_amount,
+        amounts_out,
+        to_internal_balance: bool = True,
+        min_amounts_out: List[int] = None,
+        **kwargs
+    ):
+        if min_amounts_out is None:
+            min_amounts_out = self.getDefaultMinAmountsOut(token_list)
+        token_list, amounts_out, min_amounts_out = self.sortTokenList(
+            w3, token_list, amounts_out, min_amounts_out
+        )
+        bpt_amount = self.convertTokenToWei("", bpt_amount)
+        user_data = self.getUserData(amounts_out, bpt_amount)
+        return (token_list, min_amounts_out, user_data, to_internal_balance)
 
 
 class WeightedPoolExitKind(enum.Enum):
-  EXACT_BPT_IN_FOR_ONE_TOKEN_OUT = ExactBptInForOneTokenOut()
-  EXACT_BPT_IN_FOR_TOKENS_OUT = ExactBptInForTokensOut()
-  BPT_IN_FOR_EXACT_TOKENS_OUT = BptInForExactTokensOut()
+    EXACT_BPT_IN_FOR_ONE_TOKEN_OUT = ExactBptInForOneTokenOut()
+    EXACT_BPT_IN_FOR_TOKENS_OUT = ExactBptInForTokensOut()
+    BPT_IN_FOR_EXACT_TOKENS_OUT = BptInForExactTokensOut()
 
-  def getExitPoolRequestTuple(self, *args, **kwargs):
-    return self.value.getExitPoolRequestTuple(*args, **kwargs)
+    def getExitPoolRequestTuple(self, *args, **kwargs):
+        return self.value.getExitPoolRequestTuple(*args, **kwargs)
