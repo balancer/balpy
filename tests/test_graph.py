521fb539c709ffac9eb40bb2be321421feefe718
8baller <tomrae030@hotmail.com>
2023-10-11 12:51:04 +0100

    added test for graph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,11 +1,12 @@
+from samples.theGraph.getPoolIds import main as get_pool_ids
 from samples.theGraph.getPools import main as get_pools
 
 TEST_NETWORK = "gnosis-chain"
 
 
-def test_main():
-    get_pools(TEST_NETWORK)
+def test_get_pool_ids():
+    get_pool_ids(TEST_NETWORK)
 
 
-def test_kovan():
+def test_get_pools():
     get_pools(TEST_NETWORK)
